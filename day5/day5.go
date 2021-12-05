f79e5f300bd5517e96700594bf58f2c43168afac
Morris Morrison <mwlltrdev@gmail.com>
2021-12-05 18:31:14 +0100

    remove unused code

--- day5/day5.go
+++ day5/day5.go
@@ -85,59 +85,35 @@ func Star2() string {
 	input := parseInput(file)
 	diagram := make([][]int, 1000)
 	for i := 0; i < len(diagram); i++ {
-
 		diagram[i] = make([]int, 1000)
-
 	}
+
 	for _, line := range input {
-		if isHorizontal(line) {
-			isIncreasing := line.From.X < line.To.X
-			if isIncreasing {
-				for i := line.From.X; i <= line.To.X; i++ {
-					diagram[line.From.Y][i] += 1
-				}
-			} else {
-				for i := line.From.X; i >= line.To.X; i-- {
-					diagram[line.From.Y][i] += 1
-				}
-			}
+		x := line.From.X
+		y := line.From.Y
+		var distance int
+		if line.From.X != line.To.X {
+			distance = int(math.Abs(float64(line.From.X - line.To.X)))
+		} else {
+			distance = int(math.Abs(float64(line.From.Y - line.To.Y)))
 		}
-		if isVertical(line) {
-			isIncreasing := line.From.Y < line.To.Y
-			if isIncreasing {
-				for i := line.From.Y; i <= line.To.Y; i++ {
-					diagram[i][line.From.X] += 1
-				}
-			} else {
-				for i := line.From.Y; i >= line.To.Y; i-- {
-					diagram[i][line.From.X] += 1
-				}
+
+		for i := 0; i <= distance; i++ {
+
+			diagram[y][x] += 1
+
+			if line.From.X < line.To.X {
+				x += 1
 			}
-		}
-		if isDiagonal(line) {
-			isXIncreasing := line.From.X < line.To.X
-			isYIncreasing := line.From.Y < line.To.Y
-
-			x := line.From.X
-			y := line.From.Y
-			distance := int(math.Abs(float64(line.From.X - line.To.X)))
-			for i := 0; i <= distance; i++ {
-
-				diagram[y][x] += 1
-
-				if isXIncreasing {
-					x += 1
-				} else {
-					x -= 1
-				}
-
-				if isYIncreasing {
-					y += 1
-				} else {
-					y -= 1
-				}
+			if line.From.X > line.To.X {
+				x -= 1
 			}
-
+			if line.From.Y < line.To.Y {
+				y += 1
+			}
+			if line.From.Y > line.To.Y {
+				y -= 1
+			}
 		}
 	}
 
@@ -145,24 +121,11 @@ func Star2() string {
 	for _, row := range diagram {
 		fmt.Println(row)
 	}
+
 	fmt.Println()
 	fmt.Println("Overlapping lines: ", countOverlappingLines(diagram))
 
 	return ""
-}
-
-func parseInput(file *os.File) []Line {
-	scanner := bufio.NewScanner(file)
-	input := make([]Line, 0)
-	var row int
-	for scanner.Scan() {
-		inputLineContent := scanner.Text()
-		fields := strings.Fields(inputLineContent)
-		line := Line{From: parsePoint(fields[0]), To: parsePoint(fields[2])}
-		input = append(input, line)
-		row++
-	}
-	return input
 }
 
 func parsePoint(text string) Point {
@@ -182,10 +145,6 @@ func isVertical(line Line) bool {
 	return line.From.X == line.To.X && line.From.Y != line.To.Y
 }
 
-func isDiagonal(line Line) bool {
-	return line.From.X != line.To.X && line.From.Y != line.To.Y
-}
-
 func isLineValid(line Line) bool {
 	return isHorizontal(line) || isVertical(line)
 }
@@ -201,4 +160,18 @@ func countOverlappingLines(diagram [][]int) int {
 	}
 
 	return count
+}
+
+func parseInput(file *os.File) []Line {
+	scanner := bufio.NewScanner(file)
+	input := make([]Line, 0)
+	var row int
+	for scanner.Scan() {
+		inputLineContent := scanner.Text()
+		fields := strings.Fields(inputLineContent)
+		line := Line{From: parsePoint(fields[0]), To: parsePoint(fields[2])}
+		input = append(input, line)
+		row++
+	}
+	return input
 }
